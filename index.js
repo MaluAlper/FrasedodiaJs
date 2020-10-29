67509fd9f39b58d136d9746c471334b39ff9d079
Maria Luiza <malualper@hotmail.com>
2020-10-29 15:32:58 -0300

    FrasedodiaJs

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const exibeFrase= (quote=result)=> {
+    const texto= document.getElementById("texto");
+    texto.innerHTML= `"${quote.quoteText}"`;
+    const autor= document.getElementById("autor");
+    autor.innerHTML= `${quote.quoteAuthor}`;
+};
+
+const urlFraseAleatoria= ()=> {
+    const urlFrase= 'https://quote-garden.herokuapp.com/api/v2/quotes/random';
+    return urlFrase;
+};
+
+const iniciar = async ()=> {
+    const url= urlFraseAleatoria();
+    const response = await fetch(url);
+    const result = await response.json();
+    exibeFrase(result.quote);
+};
+document.addEventListener('DOMContentLoaded', iniciar);
